46203e2051c5fd6c5dfe70e00d1fb4c8bbe4f39c
yankaizhang20 <yankaizhang2020@163.com>
2022-10-28 00:56:08 +0800

    lab1

--- labcodes/lab1/tools/sign.c
+++ labcodes/lab1/tools/sign.c
@@ -5,32 +5,32 @@
 
 int
 main(int argc, char *argv[]) {
-    struct stat st;
+    struct stat st;  //描述linux文件系统文件属性
     if (argc != 3) {
         fprintf(stderr, "Usage: <input filename> <output filename>\n");
         return -1;
     }
-    if (stat(argv[1], &st) != 0) {
+    if (stat(argv[1], &st) != 0) { //stat()获得指定文件的属性
         fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
         return -1;
     }
-    printf("'%s' size: %lld bytes\n", argv[1], (long long)st.st_size);
+    printf("'%s' size: %lld bytes\n", argv[1], (long long)st.st_size);  //st_size文件字节数
     if (st.st_size > 510) {
         fprintf(stderr, "%lld >> 510!!\n", (long long)st.st_size);
         return -1;
     }
     char buf[512];
     memset(buf, 0, sizeof(buf));
-    FILE *ifp = fopen(argv[1], "rb");
+    FILE *ifp = fopen(argv[1], "rb");    //r以只读模式打开，b表示为二进制文件
     int size = fread(buf, 1, st.st_size, ifp);
     if (size != st.st_size) {
         fprintf(stderr, "read '%s' error, size is %d.\n", argv[1], size);
         return -1;
     }
     fclose(ifp);
-    buf[510] = 0x55;
+    buf[510] = 0x55;  //0x55AA为分区结尾标识
     buf[511] = 0xAA;
-    FILE *ofp = fopen(argv[2], "wb+");
+    FILE *ofp = fopen(argv[2], "wb+");  //w+ 读写
     size = fwrite(buf, 1, 512, ofp);
     if (size != 512) {
         fprintf(stderr, "write '%s' error, size is %d.\n", argv[2], size);
